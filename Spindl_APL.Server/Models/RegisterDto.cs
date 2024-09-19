064ce1e24a7267f12285d0503c428cb99dba2ed9
Filip-d-v <filipdvarga@gmail.com>
2024-09-19 10:29:04 +0200

    Test password hasher and RegisterDto will throw in createuser controller

---
+++ Spindl_APL.Server/Models/RegisterDto.cs
@@ -0,0 +1,7 @@
+namespace Spindl_APL.Server.Models
+    public class RegisterDto
+{
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
