2f25ae26df7d7f1ba78e1a6fa4c152d1a9279919
mattiasvlex <mattias.vinje@gmail.com>
2024-09-27 10:05:38 +0200

    Fetch now working for search end point

--- spindl_apl.client/src/App.jsx
+++ spindl_apl.client/src/App.jsx
@@ -12,56 +12,23 @@ import Register from './pages/register/Register';
 import NotFound from './pages/404/NotFound';
 
 function App() {
-    const [internships, setInternships] = useState();
-
-    useEffect(() => {
-        loadInternshipData();
-    }, []);
-
-    const contents = internships === undefined
-        ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
-        : <table className="table table-striped" aria-labelledby="tableLabel">
-            <thead>
-                <tr>
-                    <th>Provider</th>
-                    <th>Location</th>
-                    <th>Number of positions</th>                    
-                </tr>
-            </thead>
-            <tbody>
-                {internships.map(internship =>
-                    <tr key={internship.provider}>
-                        <td>{internship.provider}</td>
-                        <td>{internship.location}</td>
-                        <td>{internship.position}</td>                        
-                    </tr>
-                )}
-            </tbody>
-        </table>;
-
-    return (
-        <div>                        
-            <BrowserRouter>    
-                <Layout>
-                    <Routes>
-                        <Route index path="/" element={<Home />} />         
-                        <Route path="/about" element={<About />} />
-                        <Route path="/contact" element={<Contact />} /> 
-                        <Route path="/register" element={<Register />} />
-                        <Route path="/login" element={<Login />} />                        
-                        <Route path="/subscribe" element={<Subscribe />} />
-                        <Route path="*" element={<NotFound />} />                            
-                    </Routes> 
-                </Layout>                     
-            </BrowserRouter>                     
-        </div>
-    );
-    
-    async function loadInternshipData() {
-        const response = await fetch('https://localhost:7127/api');
-        const data = await response.json();
-        setInternships(data);
-    }
+  return (
+    <div>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route index path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/subscribe" element={<Subscribe />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- spindl_apl.client/src/components/searchbar/SearchBar.jsx
+++ spindl_apl.client/src/components/searchbar/SearchBar.jsx
@@ -1,120 +1,112 @@
-import { useState, useEffect } from "react";
-import "./SearchBar.css";
-import "../../css/Buttons.css";
-import data from './internships.json';
+import { useState } from 'react';
+import './SearchBar.css';
+import '../../css/Buttons.css';
 
-//Get data from API
-/*
-const [data, setData] = React.useState(null);
-const fetchedData ="https://localhost:7127/api/company/search";
-
-  React.useEffect(() => {
-    fetch(fetchedData)
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
-
-  return (
-    <div >
-      <p>{!data ? "Loading..." : data}</p>
-      (<Table data = {data}/>)
-    </div>
-  );
-} */
-
-//Display local json data
 function SearchBar({ onSearch }) {
-  const CHAR = data;
-  const [provider, setProvider] = useState('');
+  const [companies, setCompanies] = useState([]);
   const [location, setLocation] = useState('');
   const [positions, setPositions] = useState('');
-  const [foundCHAR, setFoundCHAR] = useState(CHAR);
- 
-  // Filtering logic for multiple search fields
-  const filter = () => {
-    const filteredResults = CHAR.filter((item) => {
-      return (
-        (provider === '' || item.provider.toLowerCase().includes(provider.toLowerCase())) &&
-        (location === '' || item.location.toLowerCase().includes(location.toLowerCase())) &&
-        (positions === '' || item.positions.toLowerCase().includes(positions.toLowerCase()))
-      );
-    });
-    setFoundCHAR(filteredResults);
-    onSearch({ provider, location, positions }); 
+
+  // Sets all properties that should be seached for
+  const getSearchTerms = () => {
+    let terms = {};
+    if (location) {
+      terms.location = location;
+    }
+    if (positions) {
+      terms.numberOfStudents = positions;
+    }
+    return terms;
   };
- 
+
+  // Search
+  const getCompanies = async () => {
+    await fetch('https://localhost:7127/api/company/search', {
+      method: 'POST',
+      body: JSON.stringify(getSearchTerms()),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        data.status ? setCompanies([]) : setCompanies(data);
+      })
+      .catch((error) => console.log(error));
+  };
+
   return (
     <div>
       <div>
-        <hr className="hr-style"/>
+        <hr className="hr-style" />
       </div>
- 
-      {/* Company */}
-      <input
+      {/* Sökfält för företag */}
+      {/*       <input
         type="search"
-		id="keywords"
         value={provider}
         onChange={(e) => setProvider(e.target.value)}
         className="input"
         placeholder="Search for a company"
-      />
- 
-      {/* Location */}
+      /> */}
+      {/* Sökfält för plats */}
       <input
         type="search"
-		id="keywords"
         value={location}
         onChange={(e) => setLocation(e.target.value)}
         className="input"
         placeholder="Search for a location"
       />
- 
-      {/* Positions */}
+      {/* Sökfält för antal positioner */}
       <input
         type="search"
-		id="keywords"
         value={positions}
         onChange={(e) => setPositions(e.target.value)}
         className="input"
         placeholder="Search by number of positions"
       />
- 
       <div>
-        <button className="btn btn-gray-blue" onClick={filter}>Search</button>
+        <button className="btn btn-gray-blue" onClick={getCompanies}>
+          Search
+        </button>
       </div>
       <div>
-        <hr className="hr-style"/>
+        <hr className="hr-style" />
       </div>
- 
-      {/* Display results */}
+      {/* Resultatlista */}
       <div>
-        {foundCHAR && foundCHAR.length > 0 ? (
-          foundCHAR.map((item) => (
-            <table>
-            <thead>
-              <tr className="tr-font">            
+        <table>
+          <thead>
+            <tr className="tr-font">
               <th scope="col">Internship Provider</th>
-              <th scope="col">Job Title</th>
               <th scope="col">Location</th>
-              <th scope="col">Positions</th>            
+              <th scope="col">Positions</th>
             </tr>
-            </thead>
+          </thead>
+          {companies.length > 0 ? (
+            companies.map((company, key) => (
+              <tbody key={key}>
+                {company.internships.map((intern, key) => (
+                  <tr key={key}>
+                    <th>{company.name}</th>
+                    <td>{company.location}</td>
+                    <td>{intern.numberOfStudents}</td>
+                  </tr>
+                ))}
+              </tbody>
+            ))
+          ) : (
             <tbody>
-            <tr>            
-              <th scope="row">{item.provider}</th>
-              <th scope="row">{item.title}</th>
-              <th scope="row">{item.location}</th>
-              <th scope="row">{item.positions}</th>
-            </tr>
+              <tr>
+                <th>No search results!</th>
+                <td></td>
+                <td></td>
+              </tr>
             </tbody>
-            </table>
-          ))
-        ) : (
-          <h3>No results found!</h3>
-        )}
+          )}
+        </table>
       </div>
     </div>
   );
 }
- 
+
 export default SearchBar;
