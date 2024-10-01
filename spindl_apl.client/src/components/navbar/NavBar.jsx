e94003f9ee54c56c673e02b7043d7ee4476d2766
MinaStormcloud <164744381+MinaStormcloud@users.noreply.github.com>
2024-10-01 16:48:32 +0200

    Update NavBar.jsx

--- spindl_apl.client/src/components/navbar/NavBar.jsx
+++ spindl_apl.client/src/components/navbar/NavBar.jsx
@@ -1,7 +1,6 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
 import './NavBar.css'; 
-
 
 const NavBar = () => {  
   
@@ -13,14 +12,13 @@ const NavBar = () => {
         <li><Link to="/contact">Contact Us</Link></li>    
         <li><Link to="/subscribe">Newsletter</Link></li>
         <li><Link to="/privacy">Privacy</Link></li>
-        <li class="icon">
-        <i class="fa fa-bars">=</i>          
+        <li className="icon">
+        {/*<i className="fa fa-bars">=</i>  */}        
               <a href="/about" onClick={()=>this.handleLinkClick()}>About Us</a>
               <a href="/contact" onClick={()=>this.handleLinkClick()}>Contact Us</a>
               <a href="/subscribe" onClick={()=>this.handleLinkClick()}>Newsletter</a>
-              <a href="/privacy" onClick={()=>this.handleLinkClick()}>Privacy</a>
-          
-      </li>                
+              <a href="/privacy" onClick={()=>this.handleLinkClick()}>Privacy</a>          
+        </li>                
       </ul>
     </div>
   );
