c4b7f24875663900451bfce41e4c0e03eab43ebd
MinaStormcloud <164744381+MinaStormcloud@users.noreply.github.com>
2024-10-08 12:56:30 +0200

    ColourCleanUp

---
+++ spindl_apl.client/src/pages/dashboard/DashboardUser.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import '../../dashboard-components/dashboard-css/Dashboard.css';
+
+export default function DashboardUser() {
+  return (
+    <div id="dashboarduser">
+      <div>        
+        <h1 className="h1">Dashboard for Users</h1>         
+      </div>   
+      <div>
+        <p className="dashboard-paragraph">
+        This section is currently under construction.
+        </p>              
+      </div>           
+    </div>
+  );
+}
