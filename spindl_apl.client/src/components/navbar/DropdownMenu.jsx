9d29b0ce4980ade1673923b82919d1a49d711c7e
MinaStormcloud <164744381+MinaStormcloud@users.noreply.github.com>
2024-10-09 11:20:43 +0200

    DropdownMenu

---
+++ spindl_apl.client/src/components/navbar/DropdownMenu.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const DropdownMenu = () => {
+  return (
+    <div className="dropdown-menu">
+      <ul>
+        <li><a href="/about" onClick={()=>this.handleLinkClick()}>About</a></li>
+        <li><a href="/contact" onClick={()=>this.handleLinkClick()}>Contact</a></li>
+        <li><a href="/subscribe" onClick={()=>this.handleLinkClick()}>Newsletter</a></li>
+        <li><a href="/privacy" onClick={()=>this.handleLinkClick()}>Privacy</a></li>
+      </ul>
+    </div>
+  );
+};
+
+export default DropdownMenu;
