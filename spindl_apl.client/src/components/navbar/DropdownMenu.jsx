18e9d62bdfd1e0300c21d738eac5240aa5e9d8dc
MinaStormcloud <164744381+MinaStormcloud@users.noreply.github.com>
2024-10-09 13:48:16 +0200

    DropdownMenuUpdate

--- spindl_apl.client/src/components/navbar/DropdownMenu.jsx
+++ spindl_apl.client/src/components/navbar/DropdownMenu.jsx
@@ -4,6 +4,7 @@ const DropdownMenu = () => {
   return (
     <div className="dropdown-menu">
       <ul>
+      <li><a href="/" onClick={()=>this.handleLinkClick()}>Home</a></li>
         <li><a href="/about" onClick={()=>this.handleLinkClick()}>About</a></li>
         <li><a href="/contact" onClick={()=>this.handleLinkClick()}>Contact</a></li>
         <li><a href="/subscribe" onClick={()=>this.handleLinkClick()}>Newsletter</a></li>
