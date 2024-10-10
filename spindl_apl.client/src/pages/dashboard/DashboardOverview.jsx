66d225badcc1cad459e022045624271f24db454d
MinaStormcloud <164744381+MinaStormcloud@users.noreply.github.com>
2024-10-10 14:11:42 +0200

    OverviewCss

--- spindl_apl.client/src/pages/dashboard/DashboardOverview.jsx
+++ spindl_apl.client/src/pages/dashboard/DashboardOverview.jsx
@@ -1,17 +1,56 @@
 import React, { useState, useEffect } from "react";
-import '../../dashboard-components/dashboard-css/Dashboard.css';
+import '../../dashboard-components/dashboard-css/DashboardOverview.css';
 
-export default function DashboardOverview() {
+export default function DashboardOverview() {  
+  const [bookingCount, setBookingCount] = useState(0);
+  const [userCount, setUserCount] = useState(0);  
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {        
+        const bookingResponse = await fetch(
+          "https://localhost:7127/api/Bookings"
+        );
+        const userResponse = await fetch("https://localhost:7127/api/Users");        
+
+        if (          
+          !bookingResponse.ok ||
+          !userResponse.ok           
+        ) {
+          throw new Error("Failed to fetch data");
+        }
+        
+        const bookingData = await bookingResponse.json();
+        const userData = await userResponse.json();        
+
+        setBookingCount(bookingData.length);
+        setUserCount(userData.length);        
+        
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        // Handle error as needed
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div id="dashboardoverview">
-      <div>        
-        <h1 className="h1">Dashboard Overview</h1>         
-      </div>   
-      <div>
-        <p className="dashboard-paragraph">
-        This section is currently under construction.
-        </p>              
-      </div>           
+    <div className="overview">
+      <div className="overview-header">
+        <h2>Overview</h2>
+      </div>
+      <div className="overview-row">
+        
+        <div className="overview-card overview-col">
+          <h5>Bookings</h5>
+          <h2>{bookingCount}</h2>
+        </div>
+        {<div className="overview-card overview-col">
+          <h5>Users</h5>
+          <h2>{userCount}</h2>
+        </div> }        
+      </div>
     </div>
   );
 }
